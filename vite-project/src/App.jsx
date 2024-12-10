2886ef1198676985e598b3927d425fa2839b258c
RajaMuhammadAliAbid <159123955+RajaMuhammadAliAbid@users.noreply.github.com>
2024-12-10 12:21:47 +0500

    Add files via upload

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Chai from "./chai"
+
+function App() {
+ const username = "chai aur code"
+
+  return (
+    <div>
+    <Chai/>
+    <h1>Hello World {username}</h1>
+    <p>test para</p>
+    </div>
+  )
+}
+
+export default App
